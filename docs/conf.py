43b91c3bda182118e9ed2001a7751fe2b5a78e3a
CliFin <139973522+IMPACTlab-CliFin@users.noreply.github.com>
2024-10-14 20:51:33 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,38 +5,25 @@ sys.path.insert(0, os.path.abspath('.'))
 project = 'CliFin'
 author = 'IMPACT Lab'
 
-extensions = []  # 확장이 필요할 경우 추가
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-# Furo 테마 설정
-html_theme = 'furo'
+# Sphinx Book Theme 사용
+html_theme = 'sphinx_book_theme'
 
-# 테마 옵션 설정 (GitHub 아이콘과 다크모드 포함)
 html_theme_options = {
-    "light_css_variables": {
-        "color-brand-primary": "#007BFF",  # 메인 색상
-        "color-brand-content": "#2E2E2E",  # 본문 색상
-    },
-    # GitHub 아이콘을 상단에 추가
-    "header_icons": [
-        {
-            "name": "GitHub",
-            "url": "https://github.com/IMPACTlab-CliFin/CliFin",  # GitHub 리포지토리 링크
-            "class": "fa fa-github",  # FontAwesome GitHub 아이콘 사용
-        },
-    ],
-    "navigation_with_keys": True,  # 키보드로 사이드바 탐색 가능
+    "repository_url": "https://github.com/IMPACTlab-CliFin/CliFin",
+    "repository_branch": "main",  # 기본 브랜치 이름으로 변경
+    "path_to_docs": "docs",       # 문서 디렉토리로 변경
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "use_fullscreen_button": True,
+    "use_download_button": True,
 }
 
-# 소스 코드 링크 표시
 html_show_sourcelink = True
-
-# Static 경로 설정
 html_static_path = ['_static']
-
-# 사용자 정의 CSS 파일 추가 (추가로 스타일을 적용할 때)
-html_css_files = [
-    'custom.css',
-]
+html_css_files = ['custom.css']
