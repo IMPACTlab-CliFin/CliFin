9e4deb56e55f3c4870ee844f3d5ecf9e06a446d6
CliFin <139973522+IMPACTlab-CliFin@users.noreply.github.com>
2024-10-14 17:36:48 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,5 +10,15 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-html_theme = 'sphinx_rtd_theme'
+# 테마 설정
+html_theme = 'furo'  # Furo 테마로 변경
+
+# 선택: 추가적인 테마 설정 (색상, 폰트 등 커스터마이징)
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#007BFF",  # 메인 색상
+        "color-brand-content": "#2E2E2E",  # 본문 색상
+    },
+}
+
 html_static_path = ['_static']
