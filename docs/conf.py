ec020c98c54bcc92c3f3d62ce57183664ebf04b2
CliFin <139973522+IMPACTlab-CliFin@users.noreply.github.com>
2024-10-14 17:11:29 +0900

    Add files via upload

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = 'Your Project Name'
+author = 'Your Name'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
